db0e1398eef5430fcd70273092a16bb66e6eca6f
murollo <murillo4493@gmail.com>
2021-04-16 19:10:35 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-diretiva';
+
+  textoBotao = "Esconder";
+  esconder = false;
+
+  pessoas = [
+    { nome: "José", idade: 18, cidade: "Santo Andre", estado: "São Paulo" },
+    { nome: "Maria", idade: 22, cidade: "Santos", estado: "São Paulo" },
+    { nome: "Elcio", idade: 51, cidade: "São Bernardo", estado: "São Paulo" },
+    { nome: "Murillo", idade: 23, cidade: "São Caetano do sul", estado: "São Paulo" },
+    { nome: "Danilo", idade: 22, cidade: "Rio de Janeiro", estado: "Rio de Janeiro" }
+    ];
+
+  alterarExibicao() {
+  this.textoBotao = this.esconder ? "Esconder" : "Exibir";
+  this.esconder = !this.esconder;
+  }
+
+  pegarCor(idade){
+    return idade >= 40 ? "red": "blue"
+  }
 }
